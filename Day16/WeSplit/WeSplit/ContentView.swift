42bb03124ce9ef3e4bec5d0e6e5c2c9beddbf38f
Manish Charhate <manish.charhate@eka.care>
2022-08-24 11:55:13 +0530

    Adds initial project for day 16

---
+++ Day16/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Manish Charhate on 24/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
