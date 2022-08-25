6cb4b209b46bdf6d7828d7def796c66c884b0853
Manish Charhate <manish.charhate@eka.care>
2022-08-25 10:40:51 +0530

    Playing with Text view

--- Day16/WeSplit/WeSplit/ContentView.swift
+++ Day16/WeSplit/WeSplit/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  var body: some View {
+    Text("Hello, world!")
+      .fontWeight(.bold)
+      .padding()
+      .foregroundColor(.gray)
+      .font(.subheadline)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
