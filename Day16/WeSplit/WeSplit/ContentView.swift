2bd31bf73510d7f0eb49f4d280238c2c482c8974
Manish Charhate <manish.charhate@eka.care>
2022-08-25 12:27:06 +0530

    Add navigation bar

--- Day16/WeSplit/WeSplit/ContentView.swift
+++ Day16/WeSplit/WeSplit/ContentView.swift
@@ -9,25 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Form {
-      Group {
-        Text("Hello, world!")
-        Text("Hello, world!")
-        Text("Hello, world!")
-        Text("Hello, world!")
-        Text("Hello, world!")
-        Text("Hello, world!")
-        Text("Hello, world!")
-        Text("Hello, world!")
-        Text("Hello, world!")
-        Text("Hello, world!")
-      }
-      Group {
-        Text("Hello, world!")
-        Text("Hello, world!")
-        Text("Hello, world!")
-        Text("Hello, world!")
-        Section {
+    NavigationView {
+      Form {
+        Group {
+          Text("Hello, world!")
+          Text("Hello, world!")
+          Text("Hello, world!")
+          Text("Hello, world!")
           Text("Hello, world!")
           Text("Hello, world!")
           Text("Hello, world!")
@@ -35,7 +23,23 @@ struct ContentView: View {
           Text("Hello, world!")
           Text("Hello, world!")
         }
+        Group {
+          Text("Hello, world!")
+          Text("Hello, world!")
+          Text("Hello, world!")
+          Text("Hello, world!")
+          Section {
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+          }
+        }
       }
+      .navigationTitle("SwiftUI Navigation Bar")
+      .navigationBarTitleDisplayMode(.inline)
     }
   }
 }
