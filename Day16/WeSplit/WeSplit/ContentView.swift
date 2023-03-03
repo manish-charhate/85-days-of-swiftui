116c50c826d8d4f79d1e8df200b00e2640425a53
Manish Charhate <manish.charhate@eka.care>
2023-03-03 18:25:46 +0530

    Completes challenge

--- Day16/WeSplit/WeSplit/ContentView.swift
+++ Day16/WeSplit/WeSplit/ContentView.swift
@@ -9,43 +9,83 @@ import SwiftUI
 
 struct ContentView: View {
   
-  @State private var tapCount = 0
-  @State private var name = ""
+  @State private var checkAmount = 0.0
+  @State private var numberOfPeople = 0
+  @State private var tipPercentage = 20
   
-  private let colors = ["Black", "Red", "Blue"]
-  @State private var selectedColor = "Red"
+  @FocusState private var isCheckAmountInFocus: Bool
+  
+  private let currencyFormat: FloatingPointFormatStyle<Double>.Currency = .currency(code: Locale.current.currencyCode ?? "USD")
+  
+  var totalPerPerson: Double {
+    let peopleCount = Double(numberOfPeople + 2)
+    return totalCheckAmount / peopleCount
+  }
+  
+  var totalCheckAmount: Double {
+    let tipSelection = Double(tipPercentage)
+
+    let tipValue = checkAmount / 100 * tipSelection
+    return checkAmount + tipValue
+  }
 
   var body: some View {
     NavigationView {
       Form {
-        Group {
-          Text("Hello, world!")
-          Text("Hello, world!")
-          Text("Hello, world!")
-          Text("Hello, world!")
-          Text("Hello, world!")
-        }
-        Group {
-          Section("@State example") {
-            Button("Tap Count: \(tapCount)") {
-              tapCount += 1
+        Section {
+
+          TextField("Amount", value: $checkAmount, format: currencyFormat)
+            .keyboardType(.decimalPad)
+            .focused($isCheckAmountInFocus)
+
+          Picker("Number of People", selection: $numberOfPeople) {
+            ForEach(2..<100) {
+              Text("\($0) people")
             }
           }
-          Section("Two way binding example") {
-            TextField("Enter your name", text: $name)
-            Text("Your name: \(name)")
-          }
-          Section("Picker with ForEach") {
-            Picker("Select Color", selection: $selectedColor) {
-              ForEach(colors, id: \.self) {
-                Text($0)
+        }
+        Section {
+
+          if #available(iOS 16.0, *) {
+            Picker("Tip Percentage", selection: $tipPercentage) {
+              ForEach(0..<101, id: \.self) {
+                Text($0, format: .percent)
+              }
+            }
+            .pickerStyle(.navigationLink)
+          } else {
+            Picker("Tip Percentage", selection: $tipPercentage) {
+              ForEach(0..<101, id: \.self) {
+                Text($0, format: .percent)
               }
             }
           }
+        } header: {
+          Text("How much tip do you want to leave?")
+        }
+
+        Section {
+          Text(totalPerPerson, format: currencyFormat)
+        } header: {
+          Text("Amount per person")
+        }
+        
+        Section {
+          Text(totalCheckAmount, format: currencyFormat)
+        } header: {
+          Text("Total Amount for check")
         }
       }
-      .navigationTitle("SwiftUI Navigation Bar")
-      .navigationBarTitleDisplayMode(.inline)
+      .navigationTitle("We Split")
+      .toolbar { 
+        ToolbarItemGroup(placement: .keyboard) {
+          Spacer()
+
+          Button("Done") {
+            isCheckAmountInFocus = false
+          }
+        }
+      }
     }
   }
 }
